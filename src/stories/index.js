5795750a2dce632369a8551e04e403b38158a4c4
hungrysquirrel <joshmaxrubinstein@gmail.com>
2017-06-26 12:20:16 -0700

    update index file with new stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { storiesOf, action, linkTo } from '@storybook/react';
 import Button from './Button';
 import Welcome from './Welcome';
+import Tooltip from './Tooltip';
+import Chip from './Chip';
 
 storiesOf('Welcome', module)
   .add('to Storybook', () => (
@@ -12,6 +14,16 @@ storiesOf('Button', module)
   .add('with text', () => (
     <Button onClick={action('clicked')}>Hello Button</Button>
   ))
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+  ));
+
+storiesOf('Tooltip', module)
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+  ));
+
+storiesOf('Chip', module)
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
   ));
